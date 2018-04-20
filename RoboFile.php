4b60cb0c709d324bab34cba830a7edfc3356b1e8
Gordon Heydon <gordon@heydon.com.au>
2018-04-20 13:33:07 +1000

    Add example for using a template directory

--- RoboFile.php
+++ RoboFile.php
@@ -5,12 +5,20 @@ class RoboFile extends \Robo\Tasks
 
   use \Heydon\Robo\Task\Twig\loadTasks;
 
-  public function template() {
+  public function templateArray() {
     $this->taskTwig()
       ->setTemplatesArray('index', 'Hello {{ name }}!')
       ->setContext('name', 'Fabien')
       ->setTemplate('index')
-      ->setDestination('readme.txt')
+      ->run();
+  }
+
+  public function templateDirectory() {
+    $this->taskTwig()
+      ->setTemplatesDirectory('./templates')
+      ->setContext('name', 'Fabien')
+      ->setTemplate('index.twig')
+      ->setDestination('index.txt')
       ->run();
   }
 }
